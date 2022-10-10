5e056ba63e8119960564e378898ed44ee9bb7aa6
antspring <80751041+antspring@users.noreply.github.com>
2022-10-10 20:02:07 +0500

    feat: added PHPDoc comments

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -14,11 +14,22 @@ class Folder extends Model
         'user_id',
     ];
 
+    /**
+     * Get all files by relationship
+
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function files()
     {
         return $this->hasMany(File::class);
     }
 
+    /**
+     * Find file by name
+
+     * @param string $fileName
+     * @return File|Model|\Illuminate\Database\Eloquent\Relations\HasMany|\LaravelIdea\Helper\App\Models\_IH_File_QB
+     */
     public function findFile(string $fileName)
     {
         return $this->files()->where('name', $fileName)->firstOrFail();
