7f9a059f3e32da33224e61ec21ff7f3a5a710667
antspring <80751041+antspring@users.noreply.github.com>
2022-10-11 01:41:04 +0500

    refactor: moved methods from FileService in trait FileServiceHelpers

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -5,12 +5,15 @@ namespace App\Services;
 use App\Http\Repository\Interfaces\FileInterface;
 use App\Models\File;
 use App\Models\Folder;
+use App\Services\Traits\FileServiceHelpers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class FileService
 {
+    use FileServiceHelpers;
+
     public function __construct(private FileInterface $repository){}
 
     /**
@@ -69,6 +72,7 @@ class FileService
     /**
      * Rename file
 
+     * @param Request $request
      * @return void
      */
     public function rename(Request $request): void
@@ -82,53 +86,5 @@ class FileService
         Storage::move($path, $folder->name . '/' . $request->new_file_name);
 
         $this->repository->update($file, $request->new_file_name);
-    }
-
-    /**
-     * Search folder
-
-     * @param Request $request
-     * @return Folder|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|\LaravelIdea\Helper\App\Models\_IH_Folder_QB|object|null
-     */
-    public function findFolder(Request $request): \Illuminate\Database\Eloquent\Model|Folder|\Illuminate\Database\Eloquent\Builder|\LaravelIdea\Helper\App\Models\_IH_Folder_QB|null
-    {
-        if (!$request->folder_name){
-            return Folder::query()->where('name', $request->user()->name)->first();
-        } else {
-            $request['folder_name'] = FolderService::configurationFolderName($request->folder_name, $request->user()->name);
-
-            return Folder::query()->where('name', $request->folder_name)->firstOrFail();
-        }
-    }
-
-    /**
-     * Check for uniqueness
-
-     * @param Folder $folder
-     * @param $fileName
-     * @return void
-     */
-    public function checkForUnique(Folder $folder, $fileName): void
-    {
-        $folder->files()->each(function ($item) use ($fileName) {
-            if ($item->name === $fileName){
-                throw new HttpException(400, 'File already exists');
-            }
-        });
-    }
-
-    /**
-     * Compose data for create model File
-
-     * @param string $name
-     * @param int $id
-     * @return array
-     */
-    public function configurationFileData(string $name, int $id): array
-    {
-        return [
-            'name' => $name,
-            'folder_id' => $id
-        ];
     }
 }
---
+++ app/Services/Traits/FileServiceHelpers.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services\Traits;
+
+use App\Models\Folder;
+use App\Services\FolderService;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+trait FileServiceHelpers
+{
+    /**
+     * Search folder
+
+     * @param Request $request
+     * @return Folder|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|\LaravelIdea\Helper\App\Models\_IH_Folder_QB|object|null
+     */
+    public function findFolder(Request $request): \Illuminate\Database\Eloquent\Model|Folder|\Illuminate\Database\Eloquent\Builder|\LaravelIdea\Helper\App\Models\_IH_Folder_QB|null
+    {
+        if (!$request->folder_name){
+            return Folder::query()->where('name', $request->user()->name)->first();
+        } else {
+            $request['folder_name'] = FolderService::configurationFolderName($request->folder_name, $request->user()->name);
+
+            return Folder::query()->where('name', $request->folder_name)->firstOrFail();
+        }
+    }
+
+    /**
+     * Check for uniqueness
+
+     * @param Folder $folder
+     * @param $fileName
+     * @return void
+     */
+    public function checkForUnique(Folder $folder, $fileName): void
+    {
+        $folder->files()->each(function ($item) use ($fileName) {
+            if ($item->name === $fileName){
+                throw new HttpException(400, 'File already exists');
+            }
+        });
+    }
+
+    /**
+     * Compose data for create model File
+
+     * @param string $name
+     * @param int $id
+     * @return array
+     */
+    public function configurationFileData(string $name, int $id): array
+    {
+        return [
+            'name' => $name,
+            'folder_id' => $id
+        ];
+    }
+}
